baa652caaa39b6e6ccc06af1615004bfa55bf283
n-pizzetta <154426149+n-pizzetta@users.noreply.github.com>
2024-07-19 11:57:10 +0200

    dd

--- hb_tite_ju_app.py
+++ hb_tite_ju_app.py
@@ -139,7 +139,7 @@ with col2:
     }
     .glowing-text {
         font-size: 2.2em;
-        color: #FAF9AC;
+        color: #fff;
         font-weight: bold; /* Gras pour mieux voir le contour */
         text-shadow: 
             -1px -1px 0 #000,  
